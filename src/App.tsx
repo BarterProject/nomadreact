fd41482952ff4be9c6a4bae0b2180d874e094425
melon0259 <melon0259@gmail.com>
2022-03-27 11:54:41 +0900

    ~3

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,21 @@ const Container = styled.div`
 const H1 = styled.h1`
   color: ${(props) => props.theme.textColor};
 `;
+interface DummyProps {
+  text: string;
+  active?: boolean;
+}
+function Dummy({ text, active = false }: DummyProps) {
+  return <h1>{text}</h1>;
+}
 function App() {
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {};
   return (
     <div>
       <Container>
+        <Dummy active text="hello" />
         <H1>pro</H1>
+        <button onClick={onClick}>Click</button>
       </Container>
     </div>
   );
