1c9666965ed726fdf07a04a5c8bd5f0fcb90946a
melon0259 <melon0259@gmail.com>
2022-03-30 09:04:21 +0900

    pricechart

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,5 +1,69 @@
-function Chart() {
-  return <h1>Chart</h1>;
+import { fetchCoinHistory } from "../api";
+import { useQuery } from "react-query";
+import Apexchart from "react-apexcharts";
+
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
+interface ChartProps {
+  coinId: string;
+}
+function Chart({ coinId }: ChartProps) {
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
+    fetchCoinHistory(coinId)
+  );
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <Apexchart
+          type="line"
+          series={[
+            {
+              name: "Price",
+              data: data?.map((price) => price.close) as number[],
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+              background: "transparents",
+            },
+            stroke: {
+              curve: "smooth",
+              width: 3,
+            },
+            grid: {
+              show: false,
+            },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              axisBorder: { show: false },
+              axisTicks: { show: false },
+              labels: { show: false },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Chart;
